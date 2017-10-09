420467e9e95e8e2bfb436771785bfdc468ff186e
jabespauya <jabpau93@gmail.com>
2017-10-09 12:25:48 +0900

    updated button color

--- stopWatch/ViewController.swift
+++ stopWatch/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     var time = 0;
     
     //timer
@@ -17,23 +19,26 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lbl: UILabel!
     
-    @IBAction func reset(_ sender: UIButton) {
-        timer.invalidate()
-        time = 0
-        lbl.text = "0"
-    }
-   
-    @IBAction func start(_ sender: UIButton) {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
-    }
-    @IBAction func Pause(_ sender: UIButton) {
-        timer.invalidate()
-    }
-    
     @objc func action()
     {
         time += 1
         lbl.text = String(time)
+    }
+    
+    @IBAction func _btnStart(_ sender: UIButton){
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func _btnStop(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func _btnReset(_ sender: UIButton){
+        timer.invalidate()
+        time = 0
+        lbl.text = "0"
     }
     
     override func viewDidLoad() {
